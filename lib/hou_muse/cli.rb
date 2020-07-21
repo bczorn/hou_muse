8c4fa04b14e426db3d5375f70876f3629a37dc28
Becky Zorn <bczorn@gmail.com>
2020-07-21 00:01:31 +0000

    cli edits

--- lib/hou_muse/cli.rb
+++ lib/hou_muse/cli.rb
@@ -10,23 +10,34 @@ class HouMuse::CLI
   end
   
   def goodbye
+    puts ""
     puts "Thank you for visiting, come back soon!"
+    puts ""
   end
   
   def list_muse
     @muse = HouMuse::Muse.today
     @muse.each.with_index(1) do |muse, i|
-      puts "#{i}. #{muse.name} - #{muse.info}"
+      puts "#{i}. #{muse.name}"
     end
     menu
   end
   
   def menu
+    puts ""
     puts "Please enter the number of the museum you would like more information on. Type LIST for the list of museums, or type EXIT."
     input = gets.strip.downcase
     
     if input.to_i > 0
-      puts @muse[input.to_i-1]
+      current_muse = @muse[input.to_i-1]
+      puts <<~DOC
+      
+      ------------------------
+      #{current_muse.name.upcase}
+      ------------------------
+      #{current_muse.info}
+      
+      DOC
       menu
     elsif input == "list"
       list_muse
