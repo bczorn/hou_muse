108e8f552f818ec14a01f106a6e9e1cc0147d5a1
Becky Zorn <bczorn@gmail.com>
2020-07-27 04:04:21 +0000

    info attribute

--- lib/hou_muse/muse.rb
+++ lib/hou_muse/muse.rb
@@ -37,18 +37,15 @@ class HouMuse::Muse
     site = Nokogiri::HTML(open(url))
     
     @name = site.css(".js-museum-name").text
-    #@info = site.css(".js-museum-description").text
-    @info = site.search('p').map(&:text) #returns node set
-    # site.at('p').text  #only returns first node. not node set
-    #site.css(".js-museum-description").children.select(&:element?)
     
-    binding.pry
-    #site.css(".js-museum-description").children.each do |i|
-    #  until i == ("\r\n{Any}")
-    #  puts i
-      #binding.pry
-    #end
-  #end
+    thingx = site.xpath("//p/preceding-sibling::*[1][name()='p']")
+    thinge = site.xpath("//p[count(../p) = 1]")
+    
+    if thingx.count == 0
+      @info = thinge[0].text
+    else
+      @info = thingx.text
+    end
   
   end
   
